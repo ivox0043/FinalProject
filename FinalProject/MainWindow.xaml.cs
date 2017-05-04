d6d0b4e0e4f3d5696c9c3a128e9c984f137fb519
13 13 <egzigo.sunrise@gmail.com>
2017-05-04 11:44:26 +0200

    v1

---
+++ FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinalProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Create a member Object
+            Member memb = new Member("Jon",
+                "Jones",
+                "Bones",
+                "american",
+                "jon.jones@gmail.com",
+                25,
+                76.23,
+                45892345,
+                true,
+                12,
+                5,
+                0,
+                12,
+                0,
+                0,
+                "Hans Andersen street");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ FinalProject/Member.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public class Member
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Nationality { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        //public DateTime DateOfBirth { get; set; }
+        public double Weight { get; set; }
+        public int TelNumber {get; set;}
+        public string Address { get; set; }
+        public Boolean FighterStatus { get; set; }
+        public int ProW { get; set; }
+        public int ProL { get; set; }
+        public int ProD { get; set; }
+        public int AmW { get; set; }
+        public int AmL { get; set; }
+        public int AmD { get; set; }
+        
+        
+
+
+
+        public Member // Member object constructor
+            (
+                   string firstName, string lastName, string nickName,
+                   string nationality, string email,
+                   /*DateTime dateOfBirth,*/ int age, double weight,
+                   int telNumber, Boolean fighterStatus,
+                   int proW, int proL, int proD,
+                   int amW, int amL, int amD, string address
+            )
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.NickName = nickName;
+            this.Nationality = nationality;
+            this.Email = email;
+            //this.DateOfBirth = dateOfBirth;
+            this.Age = age;
+            this.Weight = weight;
+            this.TelNumber = telNumber;
+            this.FighterStatus = fighterStatus;
+            this.ProW = proW;
+            this.ProL = proL;
+            this.ProD = proD;
+            this.AmD = amW;
+            this.AmL = amL;
+            this.AmD = amD;
+            this.Address = address;
+        }
+    } 
+    
+}
